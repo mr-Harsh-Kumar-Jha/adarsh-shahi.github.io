a08035bfdec54c694f481b35b6a1e75cfbc64359
adarsh-shahi <adarshshahi1009@gmail.com>
2022-10-01 17:20:29 +0530

    page nav

--- javascript/script.js
+++ javascript/script.js
@@ -1,58 +1,66 @@
-'use strict';
+"use strict";
 
 ///////////////////////////////////////
 // Modal window
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelector('.btn--show-modal');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelector(".btn--show-modal");
 
 const openModal = function (e) {
-  e.preventDefault()
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+	e.preventDefault();
+	modal.classList.remove("hidden");
+	overlay.classList.remove("hidden");
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+	modal.classList.add("hidden");
+	overlay.classList.add("hidden");
 };
 
-btnsOpenModal.addEventListener('click', openModal)
+btnsOpenModal.addEventListener("click", openModal);
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  } 
+document.addEventListener("keydown", function (e) {
+	if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+		closeModal();
+	}
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1Projects = document.querySelector('#section--1')
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1Projects = document.querySelector("#section--1");
 
-btnScrollTo.addEventListener('click', e => {
-  // e.target == btnScrollTo (the element itself)
-  console.log(`****************************`);
-  e.preventDefault()
-  const s1coords = section1Projects.getBoundingClientRect()
-  console.log(e.target.getBoundingClientRect());
-  console.log(s1coords);
+btnScrollTo.addEventListener("click", (e) => {
+	// e.target == btnScrollTo (the element where the event happens itself)
+	// console.log(`****************************`);
+	// const s1coords = section1Projects.getBoundingClientRect();
+	// console.log(e.target.getBoundingClientRect());
+	// console.log(s1coords);
 
-  // current scroll positions from top of page
-  console.log(`Current Scroll(X/Y) ${window.pageXOffset} ${window.pageYOffset}`);
-  // window.scrollTo(s1coords.x + window.pageXOffset, s1coords.y + window.pageYOffset) 
-  
-  // window.scrollTo({
-  //   left: s1coords.x + window.pageXOffset,
-  //   top: s1coords.y + window.pageYOffset,
-  //   behavior: 'smooth'
-  // })
+	// current scroll positions from top of page
+	// console.log(
+	// 	`Current Scroll(X/Y) ${window.pageXOffset} ${window.pageYOffset}`
+	// );
+	// window.scrollTo(s1coords.x + window.pageXOffset, s1coords.y + window.pageYOffset)
 
-  section1Projects.scrollIntoView({behavior: 'smooth'})
+	// window.scrollTo({
+	//   left: s1coords.x + window.pageXOffset,
+	//   top: s1coords.y + window.pageYOffset,
+	//   behavior: 'smooth'
+	// })
 
+	section1Projects.scrollIntoView({ behavior: "smooth" });
+});
+
+
+document.querySelectorAll('.nav__link').forEach(ele => {
+  ele.addEventListener('click', function(e)  {
+    e.preventDefault();
+    const id = this.getAttribute('href')
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+  })
 })
 
- 
