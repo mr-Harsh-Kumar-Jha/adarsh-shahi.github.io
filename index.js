82a001db322deb9c39cddd9691bde867c0641d55
adarsh-shahi <adarshshahi1009@gmail.com>
2023-01-06 18:33:56 +0530

    scrolling to specific section

--- index.js
+++ index.js
@@ -6,29 +6,45 @@ const fetchData = async () => {
 const renderProjects = async () => {
 	const projects = await fetchData();
 	projects.forEach((project) => {
-    console.log(project);
-		document.querySelector(".container").appendChild(projectTemplate(project))
+		document
+			.querySelector(".projects-section .container")
+			.appendChild(projectTemplate(project));
 	});
 };
 
 const projectTemplate = (data) => {
-  const div = document.createElement('div')
-  div.classList.add('display')
-  div.classList.add('grid-4-section')
-  div.innerHTML =  `
+	const div = document.createElement("div");
+	div.classList.add("display");
+	div.classList.add("grid-4-section");
+	div.innerHTML = `
 						<h1 class="number">${data.id}</h1>
 						<h2 class="title">${data.title}</h2>
 						<div class="description">
 							<h4 class="description-content">${data.description}
 							</h4>
 							<div class="navigation">
-                <a class="btn" href="${data.live}">See Live</a>
-								<a class="btn" href="${data.sourceCode}">Source Code</a>
+                <a target="_blank" class="btn" href="${data.live}">See Live</a>
+								<a target="_blank" class="btn" href="${data.sourceCode}">Source Code</a>
 							</div>
 						</div>
 						<img class="desc-img" src="${data.img}" alt="" />
   `;
-  return div
+	return div;
 };
 
-renderProjects();
+renderProjects()
+
+document.querySelector('.projects').addEventListener('click', e => {
+	e.preventDefault()
+	document.querySelector('.projects-heading').scrollIntoView({
+		behavior: "smooth"
+	})
+})
+
+
+document.querySelector('.about').addEventListener('click', e => {
+	e.preventDefault()
+	document.querySelector('.about-heading').scrollIntoView({
+		behavior: "smooth"
+	})
+})
